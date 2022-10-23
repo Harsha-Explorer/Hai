4c1355946bbdeaacfd751c9d206d292df941f9fb
Harsha <mycreations2015@gmail.com>
2022-10-24 00:42:10 +0530

    getting started

---
+++ src/main/java/com/app/hai/HaiApplication.java
@@ -0,0 +1,13 @@
+package com.app.hai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/hai/HaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.hai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
